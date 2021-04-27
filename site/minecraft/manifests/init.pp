f9ea02f4d18e5e3fcb35851654e8cbeffae3006e
Daniel Lind <daniel.lindh86@hotmail.se>
2021-04-27 14:43:31 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -11,7 +11,7 @@ class minecraft {
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
-    ensure => 'eula=TRUE',
+    source => 'eula=TRUE',
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
